e484a747606e1c1799885a0662383c506abc313e
berkanhoke <berkan.hoke@agsenze.com>
2024-01-22 12:53:29 +0000

    SVM inference multiclass added

--- exported_models/classification/SVM_config.h
+++ exported_models/classification/SVM_config.h
@@ -1,13 +1,19 @@
 #ifndef SVM_CONFIG_H_INCLUDED
 #define SVM_CONFIG_H_INCLUDED
-#define NUM_CLASSES 2
-#define NUM_INTERCEPTS 1
+#define NUM_CLASSES 3
+#define NUM_INTERCEPTS 3
 #define NUM_FEATURES 2
-#define NUM_SV 293
-extern const char *LABELS[NUM_CLASSES];
+#define NUM_SV 300
+enum KernelType{
+	LINEAR,
+	POLY,
+	RBF
+};
 extern const float coeffs[NUM_CLASSES - 1][NUM_SV];
 extern const float SV[NUM_SV][NUM_FEATURES];
 extern const float intercepts[NUM_INTERCEPTS];
 extern const float w_sum[NUM_CLASSES + 1];
 extern const float svm_gamma;
+extern const float coef0;
+extern const int degree;
 #endif
