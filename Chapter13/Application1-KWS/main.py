faf9a9669b3bf3b3659a1fe6714470e0fa3d5224
bhoke <berkanh@gmail.com>
2024-09-23 21:35:17 +0100

    file formatted

--- Chapter13/Application1-KWS/main.py
+++ Chapter13/Application1-KWS/main.py
@@ -8,13 +8,19 @@ from ..PythonScripts.resnet import ResNet
 
 train_ds, val_ds, test_ds, input_shape = create_datasets(8000, 512, 256, 32)
 kws_cnn_model = ResNet(10, input_shape, 8)
-kws_cnn_model.compile(optimizer = optimizers.Adam(0.0005, weight_decay = 1e-6),
-                      loss = losses.SparseCategoricalCrossentropy(),
-                       metrics="accuracy")
-model_cp_callback = ModelCheckpoint(os.path.join(KERAS_MODEL_DIR, "kws_cnn.h5"),save_best_only=True)
+kws_cnn_model.compile(
+    optimizer=optimizers.Adam(0.0005, weight_decay=1e-6),
+    loss=losses.SparseCategoricalCrossentropy(),
+    metrics="accuracy",
+)
+model_cp_callback = ModelCheckpoint(
+    os.path.join(KERAS_MODEL_DIR, "kws_cnn.h5"), save_best_only=True
+)
 es_callback = EarlyStopping(verbose=1, patience=5)
-kws_cnn_model.fit(train_ds,
-              epochs=50,
-              validation_data= val_ds,
-              verbose=1, 
-              callbacks = [model_cp_callback, es_callback])
+kws_cnn_model.fit(
+    train_ds,
+    epochs=50,
+    validation_data=val_ds,
+    verbose=1,
+    callbacks=[model_cp_callback, es_callback],
+)
