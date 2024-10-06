e4b8f7e069f7892d045b8be2042ac95070232a93
bhoke <berkanh@gmail.com>
2024-10-06 21:46:55 +0100

    Updated Ch8 and Readme

--- Chapter08/Application2-ImageQuantization/main.py
+++ Chapter08/Application2-ImageQuantization/main.py
@@ -1,10 +1,13 @@
+import os
 import cv2
 from sklearn2c import Kmeans
+from Data.paths import RGB_IMAGE_PATH
+from Models.paths import CLUSTERING_EXPORT_DIR
 
-img = cv2.imread("im_rgb.jpg")
+img = cv2.imread(RGB_IMAGE_PATH)
 
 img_flat = img.reshape(-1,3)
 kmeans = Kmeans()
 
 kmeans.train(img_flat)
-kmeans.export("image_quantization")
+kmeans.export(os.path.join(CLUSTERING_EXPORT_DIR,"rgb_quant_kmeans"))
