92e4db08a920c9746cd15890c19a7dc4640d0f1d
bhoke <berkanh@gmail.com>
2024-10-26 16:21:55 +0100

    Models uploaded

--- Chapter13/Application1-KWS/convert_lite.py
+++ Chapter13/Application1-KWS/convert_lite.py
@@ -2,6 +2,7 @@ import os
 import tensorflow as tf
 import keras
 from Common.tflite2cc import convert_tflite2cc
+from .data_loader import create_datasets
 from Models.paths import TFLITE_MODEL_DIR, TFLITE_EXPORT_DIR, KERAS_MODEL_DIR
 
 model_path = os.path.join(KERAS_MODEL_DIR, "kws_cnn.h5")
@@ -9,8 +10,17 @@ tflite_model_path = os.path.join(TFLITE_MODEL_DIR, "kws_cnn.tflite")
 tflite_export_path = os.path.join(TFLITE_EXPORT_DIR, "kws_cnn")
 
 kws_cnn_model = keras.models.load_model(model_path)
+train_ds, *_ = create_datasets(8000, 512, 256, 32)
+def representative_dataset():
+    for input_value in train_ds.take(100):
+        yield [input_value[0]]
+
 converter = tf.lite.TFLiteConverter.from_keras_model(kws_cnn_model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.int8  # or tf.uint8
+converter.inference_output_type = tf.int8  # or tf.uint8
 kws_cnn_lite = converter.convert()
 
 with open(tflite_model_path, "wb") as tflite_file:
---
+++ Models/clustering_exports/dbscan_clus_config.c
@@ -0,0 +1,9 @@
+#include "dbscan_clus_config.h"
+const float core_points[NUM_CORE_POINTS][NUM_FEATURES] = {{-2.70255383,-4.72532919},
+ { 3.07746047, 3.54288774},
+ {-2.52044711,-2.8697942 },
+ ...,
+ {-4.60280825,-1.74693596},
+ { 4.39646191, 5.49393111},
+ {-2.07285632,-2.22146536}};
+const int labels[NUM_CORE_POINTS] = {0,0,0,...,0,0,0};
